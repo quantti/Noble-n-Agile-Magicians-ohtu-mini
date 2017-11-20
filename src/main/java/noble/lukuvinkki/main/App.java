7b6b2d321a44cc492b5765bea1edb954b203a8ae
Kari Vänttinen <kvanttinen@gmail.com>
2017-11-20 22:16:15 +0200

    käyttis fix

--- src/main/java/noble/lukuvinkki/main/App.java
+++ src/main/java/noble/lukuvinkki/main/App.java
@@ -1,15 +1,9 @@
 package noble.lukuvinkki.main;
 
-<<<<<<< HEAD
 import noble.lukuvinkki.io.KayttoliittymaIO;
 import java.util.List;
 import java.util.Scanner;
 
-=======
-import java.util.Scanner;
-
-
->>>>>>> ed971c3651b99ae7f25999b58314c968524bb6d3
 public class App {
     public static KayttoliittymaIO kayttisIO;
 
@@ -61,17 +55,11 @@ public class App {
         }
     }
 
-<<<<<<< HEAD
     private static void listaaKaikkiVinkit() {
         List kaikkiVinkit = kayttisIO.haeKaikkiVinkit();
         for (Object object : kaikkiVinkit) {
             System.out.println(object);
         }
     }
-=======
-public static void main(String[] args) throws Exception {
-    System.out.println("Hello");
-  }
 
->>>>>>> ed971c3651b99ae7f25999b58314c968524bb6d3
 }
