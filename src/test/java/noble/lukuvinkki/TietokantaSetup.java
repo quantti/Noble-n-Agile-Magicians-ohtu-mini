18c5ab56ce30af377630f959bac4d1d6ae0e86f5
Tiira Taneli J <taneli.tiira@helsinki.fi>
2017-12-07 02:43:20 +0200

    testausta

--- src/test/java/noble/lukuvinkki/TietokantaSetup.java
+++ src/test/java/noble/lukuvinkki/TietokantaSetup.java
@@ -2,25 +2,27 @@ package noble.lukuvinkki;
 
 import noble.lukuvinkki.dao.Tietokanta;
 import java.sql.SQLException;
+import java.util.List;
+import java.util.ArrayList;
 
 public class TietokantaSetup {
 
     public static Tietokanta alustaTestiTietokanta(String osoite) {
         try {
             Tietokanta tietokanta = new Tietokanta(osoite);
-            String sql1 = "DROP TABLE IF EXISTS kirja_vinkki;";
-            String sql2 = "DROP TABLE IF EXISTS video_vinkki";
-            String sql3 = "DROP TABLE IF EXISTS podcast_vinkki";
-            String sql4 = "CREATE TABLE kirja_vinkki (id INTEGER PRIMARY KEY, kirjan_nimi TEXT, kirjan_kirjoittaja TEXT);";
-            String sql5 = "CREATE TABLE video_vinkki (id INTEGER PRIMARY KEY, videon_nimi TEXT, videon_url TEXT)";
-            String sql6 = "CREATE TABLE podcast_vinkki (id INTEGER PRIMARY KEY, podcastin_nimi TEXT, podcastin_url TEXT)";
-            tietokanta.yhteys().createStatement().execute(sql1);
-            tietokanta.yhteys().createStatement().execute(sql2);
-            tietokanta.yhteys().createStatement().execute(sql3);
-            tietokanta.yhteys().createStatement().execute(sql4);
-            tietokanta.yhteys().createStatement().execute(sql5);
-            tietokanta.yhteys().createStatement().execute(sql6);
-            return tietokanta;
+	    List<String> sql = new ArrayList<>();
+            sql.add("DROP TABLE IF EXISTS kirja_vinkki;");
+            sql.add("DROP TABLE IF EXISTS video_vinkki");
+            sql.add("DROP TABLE IF EXISTS podcast_vinkki");
+	    sql.add("DROP TABLE IF EXISTS vinkki");
+	    sql.add("CREATE TABLE vinkki (id INTEGER PRIMARY KEY, otsikko TEXT, kuvaus TEXT)");
+            sql.add("CREATE TABLE kirja_vinkki (id INTEGER PRIMARY KEY, kirjan_nimi TEXT, kirjan_kirjoittaja TEXT);");
+            sql.add("CREATE TABLE video_vinkki (id INTEGER PRIMARY KEY, videon_nimi TEXT, videon_url TEXT)");
+            sql.add("CREATE TABLE podcast_vinkki (id INTEGER PRIMARY KEY, podcastin_nimi TEXT, podcastin_url TEXT)");
+	    for (String s : sql) {
+		tietokanta.yhteys().createStatement().execute(s);
+	    }
+	    return tietokanta;
         } catch (SQLException e) {
             e.printStackTrace();
             return null;
