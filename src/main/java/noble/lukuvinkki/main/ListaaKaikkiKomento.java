b58277a820ade73649fccf96a7b20eeeec438a49
Kari Vänttinen <kari.vanttinen@helsinki.fi>
2017-12-14 13:14:07 +0200

    koodin siistimistä

--- src/main/java/noble/lukuvinkki/main/ListaaKaikkiKomento.java
+++ src/main/java/noble/lukuvinkki/main/ListaaKaikkiKomento.java
@@ -1,4 +1,3 @@
-
 package noble.lukuvinkki.main;
 
 import java.sql.SQLException;
@@ -7,26 +6,21 @@ import noble.lukuvinkki.io.IO;
 import noble.lukuvinkki.io.KayttoliittymaInterface;
 import noble.lukuvinkki.tietokohteet.Vinkki;
 
-
 public class ListaaKaikkiKomento extends Komento {
 
     public ListaaKaikkiKomento(String nimi, String komento, String teksti, IO io, KayttoliittymaInterface kayttisIO) {
         super(nimi, komento, teksti, io, kayttisIO);
     }
 
-
-
-
-    
     @Override
     public void komento() throws SQLException {
-            List<Vinkki> kaikkiVinkit = kayttisIO.haeKaikkiVinkit();
-            if (tarkistaOnkoListaTyhjaTaiNull(kaikkiVinkit)) {
-                return;
-            }
-            for (Vinkki vinkki : kaikkiVinkit) {
-                io.tulosta(vinkki.toString());
-            }
+        List<Vinkki> kaikkiVinkit = kayttisIO.haeKaikkiVinkit();
+        if (tarkistaOnkoListaTyhjaTaiNull(kaikkiVinkit)) {
+            return;
+        }
+        for (Vinkki vinkki : kaikkiVinkit) {
+            io.tulosta(vinkki.toString());
+        }
 
     }
 }
--- src/main/java/noble/lukuvinkki/main/PoistaVideo.java
+++ src/main/java/noble/lukuvinkki/main/PoistaVideo.java
@@ -9,27 +9,27 @@ import noble.lukuvinkki.tietokohteet.Vinkki;
  *
  * @author emil
  */
-public class PoistaVideo extends Komento{
-    
+public class PoistaVideo extends Komento {
+
     public PoistaVideo(String nimi, String komento, String teksti, IO io, KayttoliittymaInterface kayttisIO) {
         super(nimi, komento, teksti, io, kayttisIO);
     }
-    
+
     @Override
     public void komento() throws SQLException {
-            int id = Integer.parseInt(io.lueRivi("Anna poistettavan vinkin id-numero:"));
-            Vinkki vinkki = kayttisIO.haeYksiVideo(id);
-            if (vinkki == null) {
-                io.tulosta("Vinkkiä ei löytynyt, tarkista id-numero");
-                return;
-            }
-            String vastaus = io.lueRivi("Haluatko varmasti poistaa vinkin " + vinkki.getNimi() + "? (k/e)");
-            if (vastaus.contentEquals("k") && kayttisIO.poistaVideo(id)) {
-                io.tulosta("Vinkki poistettu");
-            } else {
-                io.tulosta("Vinkkiä ei poistettu");
-            }
+        int id = Integer.parseInt(io.lueRivi("Anna poistettavan vinkin id-numero:"));
+        Vinkki vinkki = kayttisIO.haeYksiVideo(id);
+        if (vinkki == null) {
+            io.tulosta("Vinkkiä ei löytynyt, tarkista id-numero");
+            return;
+        }
+        String vastaus = io.lueRivi("Haluatko varmasti poistaa vinkin " + vinkki.getNimi() + "? (k/e)");
+        if (vastaus.contentEquals("k") && kayttisIO.poistaVideo(id)) {
+            io.tulosta("Vinkki poistettu");
+        } else {
+            io.tulosta("Vinkkiä ei poistettu");
+        }
 
     }
-    
+
 }
